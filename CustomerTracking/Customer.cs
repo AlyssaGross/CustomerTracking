bde619567dc502fde237eb045857cddf1e490486
AlyssaGross <alyssa.gross@trojans.dsu.edu>
2020-04-20 22:41:19 -0500

    10:41 4/20

--- CustomerTracking/Customer.cs
+++ CustomerTracking/Customer.cs
@@ -10,7 +10,6 @@ namespace CustomerTracking
 {
     class Customer
     {
-        //static int numberCustomers=0;
         static List<Customer> _customerList = new List<Customer> { };
 
         string _fname;
@@ -60,27 +59,72 @@ namespace CustomerTracking
         {
 
             StreamReader customerFile = new StreamReader("Customers.txt");
-            string customerLine = "";
-            string[] customerElements;
 
-            string fname, lname, phoneNo, email;
-            string[] birthdayElements;
-            int day, month, year;
+            string[] custElems, dateElems;
+            string customerLine, fname, lname, phoneNo, email;
+            DateTime birthday;
+            DateTime? customerSince, lastPurchase;
+            int points;
+            bool receiveTexts, receiveEmails, hasCard;
 
             while ((customerLine = customerFile.ReadLine()) != null)
             {
 
-                customerElements = customerLine.Split(',');
-                birthdayElements = customerElements[2].Split('/');
-                fname = customerElements[0];
-                lname = customerElements[1];
-                month = int.Parse(birthdayElements[0]);
-                day = int.Parse(birthdayElements[1]);
-                year = int.Parse(birthdayElements[2]);
-                phoneNo = customerElements[3];
-                email = customerElements[4];
-                //Customer temp = new Customer(fname, lname, new DateTime(year, month, day), phoneNo, email);
+                custElems = customerLine.Split(',');
+                fname = custElems[0];
+                lname = custElems[1];
+                dateElems = custElems[2].Split('/');
+                if (dateElems.Length == 3) {
+                    birthday = new DateTime(int.Parse(dateElems[2]), int.Parse(dateElems[0]), int.Parse(dateElems[1]));
+                }
+                else{
+                    birthday = new DateTime(1, 1, 1);
+                }
+                phoneNo = custElems[3];
+                email = custElems[4];
+                dateElems = custElems[5].Split('/');
+                if (dateElems.Length == 3)
+                {
+                    customerSince = new DateTime(int.Parse(dateElems[2]), int.Parse(dateElems[0]), int.Parse(dateElems[1]));
+                }
+                else
+                {
+                    customerSince = null;
+                }
+                dateElems = custElems[6].Split('/');
+                if (dateElems.Length == 3)
+                {
+                    lastPurchase = new DateTime(int.Parse(dateElems[2]), int.Parse(dateElems[0]), int.Parse(dateElems[1]));
+                }
+                else
+                {
+                    lastPurchase = null;
+                }
+                points = int.Parse(custElems[7]);
+                receiveTexts = bool.Parse(custElems[8]);
+                receiveEmails = bool.Parse(custElems[9]);
+                hasCard = bool.Parse(custElems[10]);
+                Customer temp = new Customer(fname, lname, birthday, phoneNo, email, customerSince,
+                                                lastPurchase, points, receiveTexts, receiveEmails, hasCard );
+            }
+        }
 
+        public static void writeCustomersToFile(){
+            string lineToWrite = "";
+            StreamWriter customerFile = new StreamWriter("Customers.txt", false);
+            foreach ( Customer c in _customerList){
+                lineToWrite = c._fname + "," + c._lname + ","
+                            + c._birthday.ToString("MM/dd/yyyy") + ","
+                            + c._phoneNo + "," + c._email + ",";
+                if(c._customerSince != null) {
+                    lineToWrite += ((DateTime)c._customerSince).ToString("MM/dd/yyyy");
+                }
+                lineToWrite += ",";
+                if (c._lastPurchase != null) {
+                    lineToWrite += ((DateTime)c._lastPurchase).ToString("MM/dd/yyyy");
+                }
+                lineToWrite += "," + c._points + "," + c._receiveTexts.ToString() + "," 
+                            + c._receiveEmails.ToString() + "," + c._hasCard.ToString();
             }
         }
 
--- CustomerTracking/Form1.cs
+++ CustomerTracking/Form1.cs
@@ -16,6 +16,15 @@ namespace CustomerTracking
         {
             InitializeComponent();
             Customer.populateCustomerList();
+            Console.WriteLine(Customer.CustomerList[0]);
+            CustomersListbox.DataSource = Customer.CustomerList;
+            CustomersListbox.DisplayMember = "LastName";
+            CustomersListbox.DisplayMember = "FirstName";
+            CustomersListbox.DisplayMember = "Birthday";
+        }
+
+        public void CustomerTrackingForm_Closing(){
+            //Customer.
         }
 
         private void CustomerTrackingForm_Load(object sender, EventArgs e)
@@ -33,16 +42,36 @@ namespace CustomerTracking
             string fname = FirstNameTextbox.Text;
             string lname = LastNameTextbox.Text;
             string[] dateArr = (BirthdayTextbox.Text).Split('/');
+            DateTime birthday;
             if (dateArr.Length == 3){
-                DateTime birthday = new DateTime(int.Parse(dateArr[2]), int.Parse(dateArr[0]), int.Parse(dateArr[1]));
+                birthday = new DateTime(int.Parse(dateArr[2]), int.Parse(dateArr[0]), int.Parse(dateArr[1]));
+            }
+            else{
+                birthday = new DateTime(1, 1, 1);
+               //some sort of error because it needs to be assigned
             }
             string email = EmailTextbox.Text;
             string phoneNo = PhoneNumberTextbox.Text;
             dateArr = (CustomerSinceTextbox.Text).Split('/');
-            DateTime customerSince = new DateTime(int.Parse(dateArr[2]), int.Parse(dateArr[0]), int.Parse(dateArr[1]));
+            DateTime? customerSince;
+            if (dateArr.Length == 3) {
+                customerSince = new DateTime(int.Parse(dateArr[2]), int.Parse(dateArr[0]), int.Parse(dateArr[1]));
+            }
+            else {
+                customerSince = null;
+            }
             dateArr = (LastPurchaseTextbox.Text).Split('/');
-            DateTime lastPurchase = new DateTime(int.Parse(dateArr[2]), int.Parse(dateArr[0]), int.Parse(dateArr[1]));
-            int points = int.Parse(PointsTextbox.Text);
+            DateTime? lastPurchase;
+            if (dateArr.Length == 3) {
+                lastPurchase = new DateTime(int.Parse(dateArr[2]), int.Parse(dateArr[0]), int.Parse(dateArr[1]));
+            }
+            else {
+                lastPurchase = null;
+            }
+            int points;
+            if (int.TryParse(PointsTextbox.Text, out points) == false ) {
+                points = 0;
+            }
             bool receiveTexts = ReceiveTextsCheckbox.Checked;
             bool receiveEmails = ReceiveEmailsCheckbox.Checked;
             bool hasCard = HasCardCheckbox.Checked;
